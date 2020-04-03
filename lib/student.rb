2c90e60ab9d65ac67df197b68dd8a0bba4a8a340
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-03 21:50:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,8 @@ class Student
     DB[:conn].execute(sql) 
   end
   
+  
+  
   def save
   if self.id
     self.update
@@ -45,9 +47,6 @@ class Student
  
   end
   
-  def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end
+
 
 end
