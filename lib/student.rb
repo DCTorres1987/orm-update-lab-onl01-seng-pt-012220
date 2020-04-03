6479e7dd4b1405e725a5101f0e5cde34f8222d6f
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-03 22:32:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,13 +4,12 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-   attr_accessor :name, :grade 
+   attr_accessor :name, :grade
    attr_reader :id
  
   def initialize(id=nil, name, grade)
   #  with DB[:conn]  	    
   @id, @name, @grade = id, name, grade
-
   end
   
     def self.create_table
