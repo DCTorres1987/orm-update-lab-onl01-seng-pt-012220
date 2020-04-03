aa6dac1630ec04e02160ab60cc854cff2add6d97
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-03 22:05:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,13 @@ class Student
     new_student
   end
   
-  
+  def self.new_from_db(row)
+    # create a new Student object given a row from the database
+      new_student = self.new
+      new_student.id = row[0]
+      new_student.name =  row[1]
+      new_student.grade = row[2]
+      new_student  # return the newly created instance
+  end
 
 end
